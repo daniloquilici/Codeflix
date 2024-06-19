a18ed4ca71be40759834199713292b1eb8f83f27
Danilo Quilici <daniloquilici@outlook.com>
2024-06-18 23:18:51 -0300

    Implementando teste de integração no CreateCategory

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using quilici.Codeflix.Application.UseCases.Category.CreateCategory;
+using quilici.Codeflix.Domain.Exceptions;
+using quilici.Codeflix.Infra.Data.EF;
+using quilici.Codeflix.Infra.Data.EF.Repositories;
+using Xunit;
+using ApplicationUseCases = quilici.Codeflix.Application.UseCases.Category.CreateCategory;
+
+namespace quilici.Codeflix.IntegrationTest.Application.UseCases.Category.CreateCategory
+{
+    [Collection(nameof(CreateCategoryTestFixture))]
+    public class CreateCategoryTest
+    {
+        private readonly CreateCategoryTestFixture _fixture;
+
+        public CreateCategoryTest(CreateCategoryTestFixture fixture) => this._fixture = fixture;
+
+        [Fact(DisplayName = nameof(CreateCategory))]
+        [Trait("Integration/Application", "CreateCategory - Use cases")]
+        public async void CreateCategory()
+        {
+            //Arrange
+            var dbcontext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbcontext);
+            var unitOfWork = new UnitOfWork(dbcontext);
+
+            var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+            var input = _fixture.GetInput();
+
+            //Action
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            //Assert
+
+            var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+
+            dbCategory.Should().NotBeNull();
+            dbCategory!.Name.Should().Be(input.Name);
+            dbCategory.Description.Should().Be(input.Description);
+            dbCategory.IsActive.Should().Be(input.IsActive);
+            dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+            output.Should().NotBeNull();
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be(input.Description);
+            output.IsActive.Should().Be(input.IsActive);
+            output.Id.Should().NotBeEmpty();
+            output.CreatedAt.Should().NotBeSameDateAs(default);
+        }
+
+        [Fact(DisplayName = nameof(CreateCategoryOnlyWithName))]
+        [Trait("Integration/Application", "CreateCategory - Use cases")]
+        public async void CreateCategoryOnlyWithName()
+        {
+            //Arrange
+            var dbcontext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbcontext);
+            var unitOfWork = new UnitOfWork(dbcontext);
+
+            var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+            var input = new CreateCategoryInput(_fixture.GetInput().Name);
+
+            //Action
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            //Assert
+
+            var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+
+            dbCategory.Should().NotBeNull();
+            dbCategory!.Name.Should().Be(input.Name);
+            dbCategory.Description.Should().Be("");
+            dbCategory.IsActive.Should().Be(true);
+            dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+            output.Should().NotBeNull();
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be("");
+            output.IsActive.Should().Be(true);
+            output.Id.Should().NotBeEmpty();
+            output.CreatedAt.Should().NotBeSameDateAs(default);
+        }
+
+        [Fact(DisplayName = nameof(CreateCategoryOnlyWithNameAndDescription))]
+        [Trait("Integration/Application", "CreateCategory - Use cases")]
+        public async void CreateCategoryOnlyWithNameAndDescription()
+        {
+            //Arrange
+            var dbcontext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbcontext);
+            var unitOfWork = new UnitOfWork(dbcontext);
+
+            var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+            var exampleInput = _fixture.GetInput();
+            var input = new CreateCategoryInput(exampleInput.Name, exampleInput.Description);
+
+            //Action
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            //Assert
+
+            var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+
+            dbCategory.Should().NotBeNull();
+            dbCategory!.Name.Should().Be(input.Name);
+            dbCategory.Description.Should().Be(input.Description);
+            dbCategory.IsActive.Should().Be(true);
+            dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+            output.Should().NotBeNull();
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be(input.Description);
+            output.IsActive.Should().Be(true);
+            output.Id.Should().NotBeEmpty();
+            output.CreatedAt.Should().NotBeSameDateAs(default);
+        }
+
+        [Theory(DisplayName = nameof(ThrowWhenCantInstantiateCategory))]
+        [Trait("Integration/Application", "CreateCategory - Use cases")]
+        [MemberData(nameof(CreateCategoryTestDataGenerator.GetInvalidInputs), parameters: 4, MemberType = typeof(CreateCategoryTestDataGenerator))]
+        public async void ThrowWhenCantInstantiateCategory(
+            CreateCategoryInput input,
+            string expectedEsceptionMessage)
+        {
+            //Arrange
+            var dbcontext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbcontext);
+            var unitOfWork = new UnitOfWork(dbcontext);
+
+            var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+            var task = async () => await useCase.Handle(input, CancellationToken.None);
+            await task.Should().ThrowAsync<EntityValidationException>().WithMessage(expectedEsceptionMessage);
+            var dbCategoriesList = _fixture.CreateDbContext(true).Categories.AsNoTracking().ToList();
+            dbCategoriesList.Should().HaveCount(0);
+        }
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,55 @@
+﻿using quilici.Codeflix.Application.UseCases.Category.CreateCategory;
+using quilici.Codeflix.IntegrationTest.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace quilici.Codeflix.IntegrationTest.Application.UseCases.Category.CreateCategory
+{
+    [CollectionDefinition(nameof(CreateCategoryTestFixture))]
+    public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> { }
+
+    public class CreateCategoryTestFixture : CategoryUseCasesBaseFixture
+    {
+        public CreateCategoryInput GetInput()
+        {
+            var category = GetExampleCategory();
+
+            return new CreateCategoryInput(category.Name, category.Description, category.IsActive);
+        }
+
+        public CreateCategoryInput GetInvalidInputShortName()
+        {
+            var invalidInputShortName = GetInput();
+            invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+
+            return invalidInputShortName;
+        }
+
+        public CreateCategoryInput GetInvalidInputLongName()
+        {
+            //Name more than 255 character
+            var invalidInputLongName = GetInput();
+            invalidInputLongName.Name = Faker.Commerce.ProductName();
+            while (invalidInputLongName.Name.Length < 255)
+                invalidInputLongName.Name += $"{invalidInputLongName.Name} {Faker.Commerce.ProductName()}";
+
+            return invalidInputLongName;
+        }
+
+        public CreateCategoryInput CreateCategoryInputInputDescriptionNull()
+        {
+            var invalidInputDescriptionNull = GetInput();
+            invalidInputDescriptionNull.Description = null!;
+            return invalidInputDescriptionNull;
+        }
+
+        public CreateCategoryInput CreateCategoryInputTooLongDescription()
+        {
+            var invalidInputTooLongDescription = GetInput();
+            invalidInputTooLongDescription.Description = Faker.Commerce.ProductDescription();
+            while (invalidInputTooLongDescription.Description.Length < 10_000)
+                invalidInputTooLongDescription.Description += $"{invalidInputTooLongDescription.Description} {Faker.Commerce.ProductDescription()}";
+
+            return invalidInputTooLongDescription;
+        }
+    }
+}
