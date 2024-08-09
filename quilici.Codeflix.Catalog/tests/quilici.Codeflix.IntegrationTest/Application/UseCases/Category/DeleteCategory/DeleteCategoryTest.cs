fca97063933ea4b5796b5d9e1c6ad6ae02b4edf4
Danilo Quilici <daniloquilici@outlook.com>
2024-08-09 11:45:00 -0300

    Integration teste Delete category

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Category/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using quilici.Codeflix.Application.Exceptions;
+using quilici.Codeflix.Infra.Data.EF;
+using quilici.Codeflix.Infra.Data.EF.Repositories;
+using System.Collections.Immutable;
+using Xunit;
+using ApplicationUseCase = quilici.Codeflix.Application.UseCases.Category.DeleteCategory;
+
+namespace quilici.Codeflix.IntegrationTest.Application.UseCases.Category.DeleteCategory
+{
+    [Collection(nameof(DeleteCategoryTestFixture))]
+    public class DeleteCategoryTest
+    {
+        private readonly DeleteCategoryTestFixture _fixture;
+
+        public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+            => _fixture = fixture;
+
+        [Fact(DisplayName = nameof(DeleteCategory))]
+        [Trait("Integration/Application", "DeleteCategory - Uses Cases")]
+        public async Task DeleteCategory()
+        {
+            //Arrange
+            var dbContext = _fixture.CreateDbContext();
+            var categoryExample = _fixture.GetExampleCategory();
+            var exampleList = _fixture.GetExampleCategoriesList(10);
+            await dbContext.AddRangeAsync(exampleList);
+            var tracking = await dbContext.AddAsync(categoryExample);
+            await dbContext.SaveChangesAsync();
+            tracking.State = EntityState.Detached;
+            var repository = new CategoryRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+            var useCase = new ApplicationUseCase.DeleteCategory(repository, unitOfWork);
+
+            var input = new ApplicationUseCase.DeleteCategoryInput(categoryExample.Id);
+
+            await useCase.Handle(input, CancellationToken.None);
+            
+            var assertDbContext = _fixture.CreateDbContext(true);
+            var dbCategoryDeleted = await assertDbContext.Categories.FindAsync(categoryExample.Id);
+
+            dbCategoryDeleted.Should().BeNull();
+            var dbCategories = await assertDbContext.Categories.ToListAsync();
+            dbCategories.Should().HaveCount(exampleList.Count);
+        }
+
+        [Fact(DisplayName = nameof(DeleteCategoryThrowWhenCategoryNotFound))]
+        [Trait("Integration/Application", "DeleteCategory - Uses Cases")]
+        public async Task DeleteCategoryThrowWhenCategoryNotFound()
+        {
+            //Arrange
+            var dbContext = _fixture.CreateDbContext();
+            var exampleList = _fixture.GetExampleCategoriesList(10);
+            await dbContext.AddRangeAsync(exampleList);
+            await dbContext.SaveChangesAsync();
+            var repository = new CategoryRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+            var useCase = new ApplicationUseCase.DeleteCategory(repository, unitOfWork);
+
+            var input = new ApplicationUseCase.DeleteCategoryInput(Guid.NewGuid());
+
+            var task = async () =>  await useCase.Handle(input, CancellationToken.None);
+            await task.Should().ThrowAsync<NotFoundException>().WithMessage($"Category '{input.Id}' not found.");
+        }
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using quilici.Codeflix.IntegrationTest.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace quilici.Codeflix.IntegrationTest.Application.UseCases.Category.DeleteCategory
+{
+    [CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+    public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
+
+    public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+    {
+    }
+}
