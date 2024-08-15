beee2d3dc70ee39f84887982fb5c7e1ca7b5d701
Danilo Quilici <daniloquilici@outlook.com>
2024-08-14 21:52:24 -0300

    Configurando as injeções de dependencia da API Catalog

---
+++ quilici.Codeflix.Catalog/src/quilici.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using quilici.Codeflix.Catalog.Application.Interfaces;
+using quilici.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using quilici.Codeflix.Catalog.Domain.Repository;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+namespace quilici.Codeflix.Catalog.Api.Configurations
+{
+    public static class UseCasesConfiguration
+    {
+        public static IServiceCollection AddUseCases(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateCategory));
+            services.AddRepositories();
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
