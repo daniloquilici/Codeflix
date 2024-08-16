9e26e35fa99a125026c4800f61f9dc985a700ee5
Danilo Quilici <daniloquilici@outlook.com>
2024-08-16 17:58:33 -0300

    Build delete test end to end

---
+++ quilici.Codeflix.Catalog/quilici.Codeflix.EndToEndTests/Api/Category/DeleteCategory/DeleteCategoryApiTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using quilici.Codeflix.Catalog.Application.UseCases.Category.Common;
+using System.Net;
+
+namespace quilici.Codeflix.Catalog.EndToEndTests.Api.Category.DeleteCategory
+{
+    [Collection(nameof(DeleteCategoryApiTestFixture))]
+    public class DeleteCategoryApiTest
+    {
+        private readonly DeleteCategoryApiTestFixture _fixture;
+
+        public DeleteCategoryApiTest(DeleteCategoryApiTestFixture fixture)
+            => _fixture = fixture;
+
+        [Fact(DisplayName = nameof(DeleteCategory))]
+        [Trait("EndToEnd/API", "Category/Delete - Endpoints")]
+        public async void DeleteCategory() 
+        {
+            //arrange
+            var exempleCategoriesList = _fixture.GetExampleCategoriesList(20);
+            await _fixture.Persistence.InsertList(exempleCategoriesList);
+            var exampleCategory = exempleCategoriesList[10];
+
+            //act
+            var (response, output) = await _fixture.ApiClient.Delete<object>($"/categories/{exampleCategory.Id}");
+
+            //assert
+            response.Should().NotBeNull();
+            response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status204NoContent);
+            output.Should().BeNull();
+            var pesistenceCategory = await _fixture.Persistence.GetById(exampleCategory.Id);
+            pesistenceCategory.Should().BeNull();
+        }
+    }
+}
---
+++ quilici.Codeflix.Catalog/quilici.Codeflix.EndToEndTests/Api/Category/DeleteCategory/DeleteCategoryApiTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using quilici.Codeflix.Catalog.EndToEndTests.Api.Category.Common;
+
+namespace quilici.Codeflix.Catalog.EndToEndTests.Api.Category.DeleteCategory
+{
+    [CollectionDefinition(nameof(DeleteCategoryApiTestFixture))]
+    public class DeleteCategoryApiTestFixtureCollection : ICollectionFixture<DeleteCategoryApiTestFixture> { }
+
+    public class DeleteCategoryApiTestFixture : CategoryBaseFixture
+    {
+    }
+}
