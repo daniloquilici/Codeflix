97ebda2354edc53e9771783a0bcb30e78464f1b1
Danilo Quilici <daniloquilici@outlook.com>
2024-09-03 16:45:01 -0300

    create new unit test

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/Genre/GenreTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/Genre/GenreTest.cs
@@ -15,6 +15,7 @@ namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.Genre
         }
 
         [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Genre - Aggregates")]
         public void Instantiate() 
         {
             var genreName = "Horror";
@@ -25,6 +26,25 @@ namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.Genre
             genre.Should().NotBeNull();
             genre.Name.Should().Be(genreName);
             genre.IsActive.Should().BeTrue();
+            genre.CreatedAt.Should().NotBeSameDateAs(default);
+            (genre.CreatedAt >= dateTimeBefore).Should().BeTrue();
+            (genre.CreatedAt <= dateTimeAfter).Should().BeTrue();
+        }
+
+        [Theory(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Genre - Aggregates")]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void InstantiateWithIsActive(bool isActive)
+        {
+            var genreName = "Horror";
+            var dateTimeBefore = DateTime.Now;
+            var genre = new DomainEntity.Genre(genreName, isActive);
+            var dateTimeAfter = DateTime.Now;
+
+            genre.Should().NotBeNull();
+            genre.Name.Should().Be(genreName);
+            genre.IsActive.Should().Be(isActive);
             genre.CreatedAt.Should().NotBeSameDateAs(default);
             (genre.CreatedAt >= dateTimeBefore).Should().BeTrue();
             (genre.CreatedAt <= dateTimeAfter).Should().BeTrue();
