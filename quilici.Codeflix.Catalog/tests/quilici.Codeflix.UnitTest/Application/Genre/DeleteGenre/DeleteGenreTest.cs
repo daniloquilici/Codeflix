8236cee49139c1cba7314338ed4200be5165b575
Danilo Quilici <daniloquilici@outlook.com>
2024-09-14 16:55:47 -0300

    Created unit test DeleteGenre

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Xunit;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Application.Genre.DeleteGenre
+{
+    [Collection(nameof(DeleteGenreTestFixture))]
+    public class DeleteGenreTest
+    {
+        private readonly DeleteGenreTestFixture _fixture;
+
+        public DeleteGenreTest(DeleteGenreTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(DeleteGenre))]
+        [Trait("Aplication", "DeleteGenre - Use cases")]
+        public async Task DeleteGenre()
+        {
+            var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+            var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+            var exampleGenre = _fixture.GetExampleGenre();
+
+            genreRepositoryMock.Setup(x => x.Get(It.Is<Guid>(x => x == exampleGenre.Id), It.IsAny<CancellationToken>())).ReturnsAsync(exampleGenre);
+
+            var useCase = new UseCase.UpdateGenre(unitOfWorkMock.Object, genreRepositoryMock.Object);
+
+            var input = new DeleteGenreInput(exampleGenre.Id);
+
+            await useCase.Handle(input, CancellationToken.None);
+
+            genreRepositoryMock.Verify(x => x.Get(It.Is<Guid>(x => x == exampleGenre.Id), It.IsAny<CancellationToken>()), Times.Once);
+            genreRepositoryMock.Verify(x => x.Delete(It.Is<DomainEntity.Genre>(x => x.Id == exampleGenre.Id), It.IsAny<CancellationToken>()), Times.Once);
+            unitOfWorkMock.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/Genre/DeleteGenre/DeleteGenreTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using quilici.Codeflix.Catalog.UnitTest.Application.Genre.Commom;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Application.Genre.DeleteGenre
+{
+    [CollectionDefinition(nameof(DeleteGenreTestFixture))]
+    public class DeleteGenreTestFixtureCollection : ICollectionFixture<DeleteGenreTestFixture> { }
+    public class DeleteGenreTestFixture : GenreUsesCaseBaseFixture
+    {
+    }
+}
