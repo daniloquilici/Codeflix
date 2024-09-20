b06be50af3b24e69786f6916807710e57781931d
Danilo Quilici <daniloquilici@outlook.com>
2024-09-19 22:12:53 -0300

    Created first test GenreRepository

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using Xunit;
+using Repository = quilici.Codeflix.Catalog.Infra.Data.EF.Repositories.GenreRepository;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Infra.Data.EF.Repositories.GenreRepository;
+[Collection(nameof(GenreRepositoryTestFixture))]
+public class GenreRepositoryTest
+{
+    private readonly GenreRepositoryTestFixture _fixture
+    public GenreRepositoryTest(GenreRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "GenreRepository - Repositories")]
+    public async Task Insert()
+    {
+        CodeFlixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleGenre = _fixture.GetExampleGenre();
+        var categoriesListExample = _fixture.GetExampleCategoriesList(3);
+        categoriesListExample.ForEach(x => exampleGenre.AddCategory(x.Id));
+        await dbContext.Categories.AddRangeAsync(categoriesListExample);
+        var genreRepository = new Repository.GenreRepository(dbContext);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        await genreRepository.Insert(exampleGenre, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var assertesDBContext = _fixture.CreateDbContext(true);
+        var dbCategory = await assertesDBContext.Categories.FindAsync(exampleGenre.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(exampleGenre.Name);
+        dbCategory.IsActive.Should().Be(exampleGenre.IsActive);
+        dbCategory.CreatedAt.Should().Be(exampleGenre.CreatedAt);
+        var genreCategoriesRelations = await assertesDBContext.GenreCategories.Where(x => x.Id = exampleGenre.Id).ToList();
+        genreCategoriesRelations.Should().HaveCount(categoriesListExample.Count);
+        genreCategoriesRelations.ForEach(relation => {
+            var expectedCategory = categoriesListExample.FirstOrDefault(x => x.Id == relation.CategoryId);
+            expectedCategory.Should().NotBeNull();
+        });
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Infra.Data.EF/Repositories/GenreRepository/GenreRepositoryTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using quilici.Codeflix.Catalog.Domain.Entity;
+using quilici.Codeflix.Catalog.IntegrationTest.Base;
+using Xunit;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Infra.Data.EF.Repositories.GenreRepository;
+
+[CollectionDefinition(nameof(GenreRepositoryTestFixture))]
+public class GenreRepositoryTestFixtureCollection : ICollectionFixture<GenreRepositoryTestFixture> { }
+
+public class GenreRepositoryTestFixture : BaseFixture
+{
+    public string GetValidGenreName()
+            => Faker.Commerce.Categories(1)[0];
+
+    public bool GetRandoBoolean() => new Random().NextDouble() < 0.5;
+
+    public DomainEntity.Genre GetExampleGenre(bool? isActive = null, List<Guid>? categoriesIds = null)
+    {
+        var genre = new DomainEntity.Genre(GetValidGenreName(), isActive ?? GetRandoBoolean());
+        categoriesIds?.ForEach(genre.AddCategory);
+        return genre;
+    }
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = string.Empty;
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public Category GetExampleCategory() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandoBoolean());
+
+    public List<Category> GetExampleCategoriesList(int length = 10) => Enumerable.Range(0, length)
+        .Select(_ => GetExampleCategory()).ToList();
+}
