e2ad805ec58179f599e9e61c316bd28db590ac21
Danilo Quilici <daniloquilici@outlook.com>
2024-10-04 16:57:52 -0300

    Build use case integration test GetGenreWithCategoryRelations

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -17,4 +17,32 @@ public class GenreUseCasesBaseFixture : BaseFixture
     }
 
     public List<DomainEntity.Genre> GetExampleListGenre(int count = 10) => Enumerable.Range(1, count).Select(_ => GetExampleGenre()).ToList();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = string.Empty;
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public DomainEntity.Category GetExampleCategory() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandoBoolean());
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10) => Enumerable.Range(0, length)
+        .Select(_ => GetExampleCategory()).ToList();
 }
--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using quilici.Codeflix.Catalog.Application.Exceptions;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Models;
 using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
 using Xunit;
 using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.GetGenre;
@@ -54,5 +55,33 @@ public class GetGenreTest
         var action = async () => await useCase.Handle(input, CancellationToken.None);
 
         await action.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre '{randomGuid}' not found.");
+    }
+
+    [Fact(DisplayName = nameof(GetGenreWithCategoryRelations))]
+    [Trait("Integratrion/Applicatrion", "GetGenre - Use cases")]
+    public async Task GetGenreWithCategoryRelations()
+    {
+        var genreExampleList = _fixture.GetExampleListGenre(10);
+        var categoriesExapleList = _fixture.GetExampleCategoriesList(5);        
+        var expectedGenre = genreExampleList[5];
+        categoriesExapleList.ForEach(category => expectedGenre.AddCategory(category.Id));
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Categories.AddRangeAsync(categoriesExapleList);
+        await dbArrangeContext.Genres.AddRangeAsync(genreExampleList);
+        await dbArrangeContext.GenresCategories.AddRangeAsync(expectedGenre.Categories.Select(categoryId => new GenresCategories(categoryId, expectedGenre.Id)));
+        await dbArrangeContext.SaveChangesAsync();
+        var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
+        var useCase = new UseCase.GetGenre(genreRepository);
+        var input = new UseCase.GetGenreInput(expectedGenre.Id);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(expectedGenre.Id);
+        output.Name.Should().Be(expectedGenre.Name);
+        output.IsActive.Should().Be(expectedGenre.IsActive);
+        output.CreatedAt.Should().Be(expectedGenre.CreatedAt);
+        output.Categories.Should().HaveCount(expectedGenre.Categories.Count);
+        output.Categories.ToList().ForEach(id => expectedGenre.Categories.Should().Contain(id));
     }
 }
