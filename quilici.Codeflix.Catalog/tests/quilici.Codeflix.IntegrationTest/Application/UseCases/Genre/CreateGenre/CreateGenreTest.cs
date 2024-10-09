5dfa0a3e4b448376ecb2c1ccc88acf6d150fc4e0
Danilo Quilici <daniloquilici@outlook.com>
2024-10-08 22:39:15 -0300

    Built integration test CreateGenre

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/CreateGenre/CreateGenreTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using quilici.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Xunit;
+using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.CreateGenre;
+
+[Collection(nameof(CreateGenreTestFixture))]
+public class CreateGenreTest
+{
+    private readonly CreateGenreTestFixture _fixture;
+
+    public CreateGenreTest(CreateGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(CreateGenre))]
+    [Trait("Integratrion/Applicatrion", "CreateGenre - Use cases")]
+    public async Task CreateGenre() 
+    {
+        CreateGenreInput input = _fixture.GetExampleInput();
+        var actDbContext = _fixture.CreateDbContext();
+        UseCase.CreateGenre createGente = new UseCase.CreateGenre(new GenreRepository(actDbContext), new UnitOfWork(actDbContext), new CategoryRepository(actDbContext));
+        var output = await createGente.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().NotBeEmpty();
+        output.Name.Should().Be(input.Name);
+        output.IsActive.Should().Be(input.IsActive);
+        output.CreatedAt.Should().NotBe(default(DateTime));
+        output.Categories.Should().HaveCount(0);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var genreFromDb = await assertDbContext.Genres.FindAsync(output.Id);
+        genreFromDb.Should().NotBeNull();
+        genreFromDb!.Name.Should().Be(input.Name);
+        genreFromDb.IsActive.Should().Be(input.IsActive);
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using quilici.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+using quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.CreateGenre;
+
+[CollectionDefinition(nameof(CreateGenreTestFixture))]
+public class CreateGenreTestFixtureCollection : ICollectionFixture<CreateGenreTestFixture> { }
+
+public class CreateGenreTestFixture : GenreUseCasesBaseFixture
+{
+    public CreateGenreInput GetExampleInput() => new CreateGenreInput(GetValidGenreName(), GetRandoBoolean());
+}
