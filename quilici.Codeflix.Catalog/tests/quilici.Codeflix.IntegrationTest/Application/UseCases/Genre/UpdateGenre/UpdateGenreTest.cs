7d4eae0c27878e90de1081b90f130491f99117a4
Danilo Quilici <daniloquilici@outlook.com>
2024-10-09 22:20:36 -0300

    Built the integration test UpdateGenre

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/UpdateGenre/UpdateGenreTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using quilici.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using quilici.Codeflix.Catalog.Application.UseCases.Genre.UpdateGenre;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Xunit;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.UpdateGenre;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.UpdateGenre;
+
+[Collection(nameof(UpdateGenreTestFixture))]
+public class UpdateGenreTest
+{
+    private readonly UpdateGenreTestFixture _fixture;
+
+    public UpdateGenreTest(UpdateGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(UpdateGenre))]
+    [Trait("Integratrion/Applicatrion", "UpdateGenre - Use cases")]
+    public async Task UpdateGenre()
+    {
+        IList<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenre(10);
+        DomainEntity.Genre targetGenre = exampleGenres[5];
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.AddRangeAsync(exampleGenres);
+        await arrangeDbContext.SaveChangesAsync();
+
+        var actDbContext = _fixture.CreateDbContext(true);
+        UseCase.UpdateGenre updateGenre = new UseCase.UpdateGenre(new UnitOfWork(actDbContext), new GenreRepository(actDbContext), new CategoryRepository(actDbContext));
+        UpdateGenreInput input = new UpdateGenreInput(targetGenre.Id, _fixture.GetValidGenreName(), !targetGenre.IsActive);
+
+        GenreModelOuput output = await updateGenre.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(targetGenre.Id);
+        output.Name.Should().Be(input.Name);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        DomainEntity.Genre? genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
+        genreFromDb.Should().NotBeNull();
+        genreFromDb!.Id.Should().Be(targetGenre.Id);
+        genreFromDb.Name.Should().Be(input.Name);
+        genreFromDb.IsActive.Should().Be((bool)input.IsActive!);
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/UpdateGenre/UpdateGenreTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.UpdateGenre;
+
+[CollectionDefinition(nameof(UpdateGenreTestFixture))]
+public class UpdateGenreTestFixtureCollection : ICollectionFixture<UpdateGenreTestFixture> { }
+
+public class UpdateGenreTestFixture : GenreUseCasesBaseFixture
+{
+}
