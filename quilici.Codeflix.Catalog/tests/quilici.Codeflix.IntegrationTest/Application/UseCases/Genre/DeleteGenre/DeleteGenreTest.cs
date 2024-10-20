9a31b1fadb674b7215363f2f720a95f3d3f3c163
Danilo Quilici <daniloquilici@outlook.com>
2024-10-20 09:11:37 -0300

    Built integration tests Delete Genre

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using quilici.Codeflix.Catalog.Application.Exceptions;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Xunit;
+using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.DeleteGenre;
+
+[Collection(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTest
+{
+    private readonly DeleteGenreTestFixture _fixture;
+
+    public DeleteGenreTest(DeleteGenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(DeleteGenre))]
+    [Trait("Integration/Application", "DeleteGenre - Uses Cases")]
+    public async Task DeleteGenre()
+    {
+        var genreExampleList = _fixture.GetExampleListGenre(10);
+        var tardetGenre = genreExampleList[5];
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genreExampleList);
+        await dbArrangeContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(new UnitOfWork(actDbContext), new GenreRepository(actDbContext));
+        var input = new UseCase.DeleteGenreInput(tardetGenre.Id);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var genreFromDb = await assertDbContext.Genres.FindAsync(tardetGenre.Id);
+        genreFromDb.Should().BeNull();
+    }
+
+    [Fact(DisplayName = nameof(DeleteGenreThrowsWhenNotFound))]
+    [Trait("Integration/Application", "DeleteGenre - Uses Cases")]
+    public async Task DeleteGenreThrowsWhenNotFound()
+    {
+        var genreExampleList = _fixture.GetExampleListGenre(10);
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genreExampleList);
+        await dbArrangeContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(new UnitOfWork(actDbContext), new GenreRepository(actDbContext));
+        
+        var randomGuid = Guid.NewGuid();
+        var input = new UseCase.DeleteGenreInput(randomGuid);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+        await action.Should().ThrowAsync<NotFoundException>().WithMessage($"Genre '{randomGuid}' not found.");
+    }
+
+    [Fact(DisplayName = nameof(DeleteGenreWithRelations))]
+    [Trait("Integration/Application", "DeleteGenre - Uses Cases")]
+    public async Task DeleteGenreWithRelations()
+    {
+        var genreExampleList = _fixture.GetExampleListGenre(10);
+        var targetGenre = genreExampleList[5];
+        var exampleCategories = _fixture.GetExampleCategoriesList(5);
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genreExampleList);
+        await dbArrangeContext.Categories.AddRangeAsync(exampleCategories);
+        await dbArrangeContext.GenresCategories.AddRangeAsync(
+                exampleCategories.Select(category => new Catalog.Infra.Data.EF.Models.GenresCategories(category.Id, targetGenre.Id))
+            );
+        await dbArrangeContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(new UnitOfWork(actDbContext), new GenreRepository(actDbContext));
+        var input = new UseCase.DeleteGenreInput(targetGenre.Id);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
+        genreFromDb.Should().BeNull();
+
+        var relations = await assertDbContext.GenresCategories.AsNoTracking().Where(relation => relation.GenreId == targetGenre.Id).ToListAsync();
+        relations.Should().HaveCount(0);
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/DeleteGenre/DeleteGenreTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.DeleteGenre;
+
+[CollectionDefinition(nameof(DeleteGenreTestFixture))]
+public class DeleteGenreTestFixtureCollection : ICollectionFixture<DeleteGenreTestFixture> { }
+public class DeleteGenreTestFixture : GenreUseCasesBaseFixture
+{
+}
