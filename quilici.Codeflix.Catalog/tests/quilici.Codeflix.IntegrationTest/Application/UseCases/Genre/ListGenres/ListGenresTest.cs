b2eefc97bdb1696900d1d9b937c296bb54c6c3ac
Danilo Quilici <daniloquilici@outlook.com>
2024-10-20 09:27:00 -0300

    Built integration test ListGenres

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/ListGenres/ListGenresTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using quilici.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using System.Runtime.InteropServices;
+using Xunit;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+using UseCase = quilici.Codeflix.Catalog.Application.UseCases.Genre.ListGenres;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.ListGenres;
+
+[Collection(nameof(ListGenresTestFixture))]
+public class ListGenresTest
+{
+    private readonly ListGenresTestFixture _fixture;
+
+    public ListGenresTest(ListGenresTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ListGenres))]
+    [Trait("Integration/Application", "ListGenres - UseCases")]
+    public async void ListGenres() 
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenre();
+        var arrangeDbContext = _fixture.CreateDbContext();
+        await arrangeDbContext.Genres.AddRangeAsync(exampleGenres);
+        await arrangeDbContext.SaveChangesAsync();
+
+        var useCase = new UseCase.ListGenres(new GenreRepository(_fixture.CreateDbContext(true)));
+        var input = new UseCase.ListGenresInput(1, 20);
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Items.Should().HaveCount(exampleGenres.Count);
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Items.ToList().ForEach(outputItem => 
+        {
+            DomainEntity.Genre? exampleItem = exampleGenres.Find(example => example.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+        });
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.IntegrationTest/Application/UseCases/Genre/ListGenres/ListGenresTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.IntegrationTest.Application.UseCases.Genre.ListGenres;
+
+[CollectionDefinition(nameof(ListGenresTestFixture))]
+public class ListGenresTestFixtureCollection : ICollectionFixture<ListGenresTestFixture> { }
+
+public class ListGenresTestFixture : GenreUseCasesBaseFixture
+{
+}
