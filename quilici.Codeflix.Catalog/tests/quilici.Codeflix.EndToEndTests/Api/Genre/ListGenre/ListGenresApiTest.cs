e7f859567bc765ccc3ad4638899be775fb94646f
Danilo Quilici <daniloquilici@outlook.com>
2024-10-25 09:58:31 -0300

    Built endtoend test ListGenresPaginated

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/Genre/ListGenre/ListGenresApiTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/Genre/ListGenre/ListGenresApiTest.cs
@@ -10,7 +10,7 @@ using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
 namespace quilici.Codeflix.Catalog.EndToEndTests.Api.Genre.ListGenre;
 
 [Collection(nameof(ListGenresApiTestFixture))]
-public class ListGenresApiTest
+public class ListGenresApiTest : IDisposable
 {
     private readonly ListGenresApiTestFixture _fixture;
 
@@ -67,5 +67,45 @@ public class ListGenresApiTest
         output.Meta.PerPage.Should().Be(input.PerPage);
         output.Meta.Total.Should().Be(0);
         output.Data!.Count.Should().Be(0);
+    }
+
+    [Theory(DisplayName = nameof(ListGenresPaginated))]
+    [Trait("EndtoEnd/Api", "Genre/ListGenres - Endpoints")]
+    [InlineData(10, 1, 5, 5)]
+    [InlineData(10, 2, 5, 5)]
+    [InlineData(7, 2, 5, 2)]
+    [InlineData(7, 3, 5, 0)]
+    public async Task ListGenresPaginated(int quantityToGenerate, int page, int perPage, int expectedQuantityItems)
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenre(quantityToGenerate);
+        var targetGenre = exampleGenres[5];
+        await _fixture.Persistence.InsertList(exampleGenres);
+
+        var input = new ListGenresInput(page, perPage);
+
+        var (response, output) = await _fixture.ApiClient.Get<TestApiResponseList<GenreModelOutput>>("/genres", input);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.OK);
+        output.Should().NotBeNull();
+        output!.Meta.Should().NotBeNull();
+        output.Data.Should().NotBeNull();
+        output.Meta!.CurrentPage.Should().Be(input.Page);
+        output.Meta.PerPage.Should().Be(input.PerPage);
+        output.Meta.Total.Should().Be(quantityToGenerate);
+        output.Data!.Count.Should().Be(expectedQuantityItems);
+        output.Data.ToList().ForEach(outputItem =>
+        {
+            var exampleItem = exampleGenres.First(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            exampleItem.Name.Should().Be(outputItem.Name);
+            exampleItem.IsActive.Should().Be(outputItem.IsActive);
+            exampleItem.CreatedAt.TrimMillisseconds().Should().Be(outputItem.CreatedAt.TrimMillisseconds());
+        });
+    }
+
+    public void Dispose()
+    {
+        _fixture.CleanPersistence();
     }
 }
