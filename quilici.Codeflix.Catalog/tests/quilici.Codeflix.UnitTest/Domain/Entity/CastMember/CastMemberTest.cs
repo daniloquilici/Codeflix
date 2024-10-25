f210bc698d8d8e43d71bbd1d0c92c2d452c02392
Danilo Quilici <daniloquilici@outlook.com>
2024-10-25 12:25:46 -0300

    Built unit test Instantiate from CastMember

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/CastMember/CastMemberTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using DomainEntiry = quilici.Codeflix.Catalog.Domain.Entity;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.CastMember;
+
+[Collection(nameof(CastMemberTestFixture))]
+public class CastMemberTest
+{
+    private readonly CastMemberTestFixture _fixture;
+
+    public CastMemberTest(CastMemberTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "CastMember - Aggregates")]
+    public void Instantiate() 
+    {
+        var dateTimeBefore = DateTime.Now.AddSeconds(-1);
+        var name = "Danilo";
+        var type = CastMemberType.Director;
+
+        var castMember = new DomainEntiry.CastMember(name, type);
+        var dateTimeAfter = DateTime.Now.AddSeconds(1);
+
+        castMember.Id.Should().NotBeNull();
+        castMember.Name.Should().Be(name);
+        castMember.Type.Should().Be(type);
+        (castMember.CreatedAt >= dateTimeBefore).Should().BeTrue();
+        (castMember.CreatedAt <=> dateTimeAfter).Should().BeTrue();
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/CastMember/CastMemberTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using quilici.Codeflix.Catalog.UnitTest.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.CastMember;
+
+[CollectionDefinition(nameof(CastMemberTestFixture))]
+public class CastMemberTestFixtureCollection : ICollectionFixture<CastMemberTestFixture> { }
+
+public class CastMemberTestFixture : BaseFixture
+{
+    public string GetValidName()
+        => Faker.Name.FullName();
+
+    public CastMemberType GetRandomCastMemberType()
+        => (new Random().Next(1, 2)) as CastMemberType;
+}
