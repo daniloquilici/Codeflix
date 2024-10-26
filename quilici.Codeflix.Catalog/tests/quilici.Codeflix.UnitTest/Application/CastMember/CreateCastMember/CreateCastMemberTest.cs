d885209446b49227b00270791a3d2cdc9f704948
Danilo Quilici <daniloquilici@outlook.com>
2024-10-26 17:52:41 -0300

    Created unit teste use case createdCastMember called ThrowsWhenInvalidName

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/CastMember/CreateCastMember/CreateCastMemberTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/CastMember/CreateCastMember/CreateCastMemberTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using quilici.Codeflix.Catalog.Application.Interfaces;
 using quilici.Codeflix.Catalog.Application.UseCases.CastMember.CreateCastMember;
+using quilici.Codeflix.Catalog.Domain.Exceptions;
 using quilici.Codeflix.Catalog.Domain.Repository;
 using Xunit;
 using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
@@ -40,5 +41,23 @@ public class CreateCastMemberTest
 
         unitOfWork.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
         repositoryMock.Verify(x => x.Insert(It.Is<DomainEntity.CastMember>(x => x.Name == input.Name && x.Type == input.Type), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Theory(DisplayName = nameof(ThrowsWhenInvalidName))]
+    [Trait("Aplication", "CreateCastMember - Use cases")]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public async Task ThrowsWhenInvalidName(string? name)
+    {
+        var input = new CreateCastMemberInput(name!, _fixture.GetRandomCastMemberType());
+
+        var repositoryMock = new Mock<ICastMemberRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+
+        var useCase = new UseCase.CreateCastMember(unitOfWork.Object, repositoryMock.Object);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+        await action.Should().ThrowAsync<EntityValidationException>().WithMessage($"Name should not be empty or null");
     }
 }
