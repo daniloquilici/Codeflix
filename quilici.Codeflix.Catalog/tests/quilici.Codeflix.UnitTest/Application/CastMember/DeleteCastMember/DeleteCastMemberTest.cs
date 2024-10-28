ac16c04675c8467961b06d3730736cbaefb5b43b
Danilo Quilici <daniloquilici@outlook.com>
2024-10-28 17:04:21 -0300

    Build unit test ThrowsWhenNotFound

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Application/CastMember/DeleteCastMember/DeleteCastMemberTest.cs
@@ -1,7 +1,9 @@
 ﻿using FluentAssertions;
 using Moq;
+using quilici.Codeflix.Catalog.Application.Exceptions;
 using quilici.Codeflix.Catalog.Application.Interfaces;
 using quilici.Codeflix.Catalog.Domain.Repository;
+using System.Linq.Expressions;
 using Xunit;
 using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
 using UseCase = quilici.Codeflix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
@@ -37,5 +39,21 @@ public class DeleteCastMemberTest
         castMemberRepositoryMock.Verify(x => x.Get(It.Is<Guid>(x => x == input.Id), It.IsAny<CancellationToken>()), Times.Once);
         castMemberRepositoryMock.Verify(x => x.Delete(It.Is<DomainEntity.CastMember>(x => x.Id == input.Id), It.IsAny<CancellationToken>()), Times.Once);
         unitOfWorkMock.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact(DisplayName = nameof(ThrowsWhenNotFound))]
+    [Trait("Application", "DeleteCastMember - Use cases")]
+    public async Task ThrowsWhenNotFound()
+    {
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var castMemberRepositoryMock = new Mock<ICastMemberRepository>();
+
+        castMemberRepositoryMock.Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ThrowsAsync(new NotFoundException("notFound"));
+
+        var input = new UseCase.DeleteCastMemberInput(Guid.NewGuid());
+        var useCase = new UseCase.DeleteCastMember(unitOfWorkMock.Object, castMemberRepositoryMock.Object);
+
+        var action = async () => await useCase.Handle(input, CancellationToken.None);
+        await action.Should().ThrowAsync<NotFoundException>();
     }
 }
