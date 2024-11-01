9265013b97cd23dbc1d63bdce52a5a540af3ca7f
Danilo Quilici <daniloquilici@outlook.com>
2024-10-31 21:37:01 -0300

    Built castmember reposritory.

--- quilici.Codeflix.Catalog/src/quilici.Codeflix.Infra.EF/Repositories/CastMemberRepository.cs
+++ quilici.Codeflix.Catalog/src/quilici.Codeflix.Infra.EF/Repositories/CastMemberRepository.cs
@@ -32,6 +32,7 @@ public class CastMemberRepository : ICastMemberRepository
     {
         var toSkip = (searchInput.Page - 1) * searchInput.PerPage;
         var query = _castMembers.AsNoTracking();
+        query = AddOrderToQuery(query, searchInput.OrderBy, searchInput.Order);
 
         if (!string.IsNullOrWhiteSpace(searchInput.Search))
             query = query.Where(x => x.Name.Contains(searchInput.Search));
@@ -39,6 +40,22 @@ public class CastMemberRepository : ICastMemberRepository
         var items = await query.Skip(toSkip).Take(searchInput.PerPage).ToListAsync();
         var count = query.Count();
         return new SearchOutput<CastMember>(searchInput.Page, searchInput.PerPage, count, items.AsReadOnly());
+    }
+
+    private IQueryable<CastMember> AddOrderToQuery(IQueryable<CastMember> query, string orderProperty, SearchOrder order)
+    {
+        var orderedeQuery = (orderProperty.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => query.OrderBy(x => x.Name).ThenBy(x => x.Id),
+            ("name", SearchOrder.Desc) => query.OrderByDescending(x => x.Name).ThenByDescending(x => x.Id),
+            ("id", SearchOrder.Asc) => query.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => query.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => query.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => query.OrderByDescending(x => x.CreatedAt),
+            _ => query.OrderBy(x => x.Name).ThenBy(x => x.Id),
+        };
+
+        return orderedeQuery.ThenBy(x => x.CreatedAt);
     }
 
     public Task Update(CastMember aggregate, CancellationToken _)
