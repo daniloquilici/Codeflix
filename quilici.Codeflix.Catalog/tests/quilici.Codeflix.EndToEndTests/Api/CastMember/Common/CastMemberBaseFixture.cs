35ce4cc7f77591590a7949f6e6198a92c49382b1
Danilo Quilici <daniloquilici@outlook.com>
2024-11-01 15:39:43 -0300

    Built envoriment test endtoend CastMember

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/Common/CastMemberBaseFixture.cs
@@ -0,0 +1,46 @@
+﻿using quilici.Codeflix.Catalog.Domain.Enum;
+using quilici.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using quilici.Codeflix.Catalog.EndToEndTests.Base;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+
+namespace quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
+public class CastMemberBaseFixture : BaseFixture
+{
+    public string GetValidName()
+    => Faker.Name.FullName();
+
+    public CastMemberType GetRandomCastMemberType()
+        => (CastMemberType)(new Random().Next(1, 2));
+
+    public DomainEntity.CastMember GetExampleCastMember()
+        => new DomainEntity.CastMember(GetValidName(), GetRandomCastMemberType());
+
+    public List<DomainEntity.CastMember> GetExampleCastMembersList(int quantity)
+        => Enumerable.Range(1, quantity).Select(_ => GetExampleCastMember()).ToList();
+
+    public List<DomainEntity.CastMember> GetExampleCastMembersListByNames(List<string> names)
+    => names.Select(name =>
+    {
+        var example = GetExampleCastMember();
+        example.Update(name, example.Type);
+        return example;
+    }).ToList();
+
+    public List<DomainEntity.CastMember> CloneListOrdered(List<DomainEntity.CastMember> castMemberList, string orderBy, SearchOrder order)
+    {
+        var listClone = new List<DomainEntity.CastMember>(castMemberList);
+
+        var orderedEnumerable = (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name).ThenBy(x => x.Id),
+            ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name).ThenByDescending(x => x.Id),
+            ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+            _ => listClone.OrderBy(x => x.Name).ThenBy(x => x.Id),
+        };
+
+        return orderedEnumerable.ToList();
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/Common/CastMemberPesistence.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quilici.Codeflix.Catalog.Infra.Data.EF;
+using DomainEntity = quilici.Codeflix.Catalog.Domain.Entity;
+
+namespace quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
+public class CastMemberPesistence
+{
+    private readonly CodeFlixCatalogDbContext _context;
+
+    public CastMemberPesistence(CodeFlixCatalogDbContext context)
+        => _context = context;
+
+    public async Task InsertList(IList<DomainEntity.CastMember> castMember)
+    {
+        await _context.CastMembers.AddRangeAsync(castMember);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<DomainEntity.CastMember?> GetById(Guid id)
+    {
+        return await _context.CastMembers.AsNoTracking().FirstOrDefaultAsync(castMember => castMember.Id == id);
+    }
+}
