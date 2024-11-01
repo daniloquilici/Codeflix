fa7bcb7a450231faad2bbd6cbb3659e43b4852c9
Danilo Quilici <daniloquilici@outlook.com>
2024-11-01 16:50:42 -0300

    Built endtoend test delete from CastMember

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/DeleteCastMember/DeleteCastMemberApiTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using quilici.Codeflix.Catalog.Api.ApiModels.Response;
+using quilici.Codeflix.Catalog.Application.UseCases.CastMember.Common;
+using quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
+using System.Net;
+
+namespace quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.DeleteCastMember;
+
+[Collection(nameof(CastMemberApiBaseFixture))]
+public class DeleteCastMemberApiTest
+{
+    private readonly CastMemberApiBaseFixture _fixture;
+
+    public DeleteCastMemberApiTest(CastMemberApiBaseFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Delete))]
+    [Trait("EndToEnd/API", "CastMembers/Delete - EndPoints")]
+    public async Task Delete()
+    {
+        var examples = _fixture.GetExampleCastMembersList(5);
+        var example = examples[2];
+        await _fixture.Persistence.InsertList(examples);
+
+        var (response, output) = await _fixture.ApiClient.Delete<object>($"castmember/{example.Id}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status204NoContent);
+        
+        var castMemberExample = await _fixture.Persistence.GetById(example.Id);
+        castMemberExample.Should().BeNull();
+    }
+}
