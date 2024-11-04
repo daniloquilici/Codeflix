f8985baa6ff45a2d275ebe2c78cfc87497eabb38
Danilo Quilici <daniloquilici@outlook.com>
2024-11-04 16:31:32 -0300

    Created test endtoent return empty when list is empty

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/ListCastMembers/ListCastMembersApiTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/ListCastMembers/ListCastMembersApiTest.cs
@@ -8,13 +8,18 @@ using System.Net;
 namespace quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.ListCastMembers;
 
 [Collection(nameof(CastMemberApiBaseFixture))]
-public class ListCastMembersApiTest
+public class ListCastMembersApiTest : IDisposable
 {
     private readonly CastMemberApiBaseFixture _fixture;
 
     public ListCastMembersApiTest(CastMemberApiBaseFixture fixture)
     {
         _fixture = fixture;
+    }
+
+    public void Dispose()
+    {
+        _fixture.CleanPersistence();
     }
 
     [Fact(DisplayName = nameof(List))]
@@ -42,5 +47,21 @@ public class ListCastMembersApiTest
             outputItem.Name.Should().Be(exampleItem!.Name);
             outputItem.Type.Should().Be(exampleItem!.Type);
         });
+    }
+
+    [Fact(DisplayName = nameof(ReturnsEmpty))]
+    [Trait("EndToEnd/API", "CastMember/List")]
+    public async Task ReturnsEmpty()
+    {
+        var (response, output) = await _fixture.ApiClient.Get<TestApiResponseList<CastMemberModelOutput>>("castmember");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output!.Should().NotBeNull();
+        output!.Meta.Should().NotBeNull();
+        output.Meta!.CurrentPage.Should().Be(1);
+        output.Meta.Total.Should().Be(0);
+        output.Data.Should().NotBeNull();
+        output!.Data.Should().HaveCount(0);
     }
 }
