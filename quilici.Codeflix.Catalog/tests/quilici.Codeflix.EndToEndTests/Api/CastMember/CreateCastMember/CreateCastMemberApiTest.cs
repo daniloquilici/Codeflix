016fb9b14955e2568eba97e655a3fdc54c3b6894
Danilo Quilici <daniloquilici@outlook.com>
2024-11-04 10:38:09 -0300

    Built test endtoend from CastMember ThrowWhenNameIsEmpty

--- quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/CreateCastMember/CreateCastMemberApiTest.cs
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.EndToEndTests/Api/CastMember/CreateCastMember/CreateCastMemberApiTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using quilici.Codeflix.Catalog.Application.UseCases.CastMember.Common;
 using quilici.Codeflix.Catalog.Application.UseCases.CastMember.CreateCastMember;
 using quilici.Codeflix.Catalog.EndToEndTests.Api.CastMember.Common;
@@ -38,5 +39,23 @@ public class CreateCastMemberApiTest
         castMemberFromDb.Should().NotBeNull();
         castMemberFromDb!.Name.Should().Be(example.Name);
         castMemberFromDb.Type.Should().Be(example.Type);
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenNameIsEmpty))]
+    [Trait("EndtoEnd/Api", "CastMember/Create")]
+    [InlineData("")]
+    [InlineData("  ")]
+    public async Task ThrowWhenNameIsEmpty(string name)
+    {
+        var example = _fixture.GetExampleCastMember();
+
+        var (response, output) = await _fixture.ApiClient.Post<ProblemDetails>("/castmember", new CreateCastMemberInput(name!, example.Type));
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status422UnprocessableEntity);
+        output.Should().NotBeNull();
+        output!.Title.Should().Be("One or more validation errors ocurred");
+        output.Detail.Should().Be("Name should not be empty or null");
+
     }
 }
