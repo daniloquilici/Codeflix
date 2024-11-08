0019eac22047b5087a5481fcb5767a14cfe627b9
Danilo Quilici <daniloquilici@outlook.com>
2024-11-08 14:01:08 -0300

    Build unit test from Video - Instantiate

---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/Video/VideoTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.Video;
+
+[Collection(nameof(VideoTestFixture))]
+public class VideoTest
+{
+    private readonly VideoTestFixture _fixture;
+
+    public VideoTest(VideoTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Video - Aggregate")]
+    public void Instantiate() 
+    {
+    
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/Entity/Video/VideoTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using quilici.Codeflix.Catalog.UnitTest.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Domain.Entity.Video;
+
+[CollectionDefinition(nameof(VideoTestFixture))]
+public class VideoTestFixtureCollection : ICollectionFixture<VideoTestFixture> { }
+
+public class VideoTestFixture : BaseFixture
+{
+}
