38bc867ef624b9182e37ba3a19e8b070fc32865e
Danilo Quilici <daniloquilici@outlook.com>
2024-11-18 16:53:45 -0300

    Built unit test and domain from value object Image

---
+++ quilici.Codeflix.Catalog/src/quilici.Codeflix.Domain/ValueObject/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace quilici.Codeflix.Catalog.Domain.ValueObject;
+public class Image
+{
+    public string Path { get; }
+
+    public Image(string path)
+    {
+        Path = path;
+    }
+}
---
+++ quilici.Codeflix.Catalog/tests/quilici.Codeflix.UnitTest/Domain/ValueObject/ImageTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using quilici.Codeflix.Catalog.Domain.ValueObject;
+using quilici.Codeflix.Catalog.UnitTest.Common;
+using Xunit;
+
+namespace quilici.Codeflix.Catalog.UnitTest.Domain.ValueObject;
+public class ImageTest : BaseFixture
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Image - ValueObjects")]
+    public void Instantiate()
+    {
+        var path = Faker.Image.PicsumUrl();
+
+        var image = new Image(path);
+
+        image.Should().NotBeNull();
+        image.Path.Should().NotBeNull();
+    }
+}
